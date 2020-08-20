92db16186435194304acc1aa146677ac6231ff6c
jun-low <zhijun.low@gmail.com>
2020-08-20 17:03:02 +0800

    add test

---
+++ index.test.js
@@ -0,0 +1,10 @@
+const process = require('process');
+const cp = require('child_process');
+const path = require('path');
+
+// shows the runner will run a javascript action with env / stdout protocol
+test('test runs', () => {
+  process.env['INPUT_MILLISECONDS'] = 500;
+  const ip = path.join(__dirname, 'index.js');
+  console.log(cp.execSync(`node ${ip}`).toString());
+})
